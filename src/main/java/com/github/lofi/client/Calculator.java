2e991a94aa660a84c306e3c7dfa3ca1917b09db4
lofidewanto <lofidewanto@gmail.com>
2019-06-02 00:33:12 +0200

    add example

--- src/main/java/com/github/lofi/client/Calculator.java
+++ src/main/java/com/github/lofi/client/Calculator.java
@@ -1,5 +1,23 @@
 package com.github.lofi.client;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+import jsinterop.annotations.JsType;
+
+@JsType(namespace = "Calculator")
 public class Calculator {
+	
+	private static Logger logger = Logger
+			.getLogger(Calculator.class.getName());
+
+	public Calculator() {
+		logger.info("Calculator...");
+	}
+
+	public int calculateSum(List<Integer> values) {
+		int sum = values.stream().mapToInt(Integer::intValue).sum();
+		return sum;
+	}
 
 }
--- src/test/java/com/github/lofi/CalculatorTest.java
+++ src/test/java/com/github/lofi/CalculatorTest.java
@@ -2,11 +2,18 @@ package com.github.lofi;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.Test;
 
 import com.github.lofi.client.Calculator;
 
 public class CalculatorTest {
+
+	private static Logger logger = Logger
+			.getLogger(CalculatorTest.class.getName());
 
 	private Calculator calculator;
 
@@ -15,7 +22,14 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testApp() {
+	public void calculateSum() {
 		assertNotNull(calculator);
+
+		List<Integer> values = new ArrayList<>();
+		values.add(12);
+		values.add(14);
+		
+		int result = calculator.calculateSum(values);
+		logger.info("Result: " + result);
 	}
 }
